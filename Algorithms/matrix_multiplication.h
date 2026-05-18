7759193fb6371119223e3c798f35ec29cc24c317
Rayyan Sohaib <rayyansohaib2004@gmail.com>
2026-05-18 09:41:11 +0000

    matrix_multiplication

--- Algorithms/matrix_multiplication.h
+++ Algorithms/matrix_multiplication.h
@@ -1,14 +1,41 @@
 #pragma once
+#include <vector>
+#include <span>
 using namespace std;
 using ll = long long;
 
 template<int N, int M, int P>
-void matmul(ll A[N][M], ll B[M][P], ll C[N][P]) {
+void matmul(ll (&A)[N][M], ll (&B)[M][P], ll (&C)[N][P]) {
     for (int i = 0; i < N; i++) {
         for (int k = 0; k < M; k++) {
+            ll A_ik = A[i][k]; if (A_ik == 0) continue;
             for (int j = 0; j < P; j++) {
-                C[i][j] += A[i][k] * B[k][j];
+                C[i][j] += A_ik * B[k][j];
             }
         }
     }
 }
+
+void matmul(span<const ll> A,span<const ll> B, span<ll> C, int n, int m, int p) {
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < m; k++) {
+            ll A_ik = A[i * m + k]; if (A_ik == 0) continue; 
+            for (int j = 0; j < p; j++) {
+                C[i * p + j] += A_ik * B[k * p + j];
+            }
+        }
+    }
+}
+void matmul(span<const ll> A,span<const ll> B, span<ll> C, int n, int m, int p, int lda, int ldb, int ldc) {}
+struct Matrix {
+    int n, m;
+    vector<ll> data;
+    Matrix(int n, int m) : n(n), m(m), data(n * m) {}
+    ll* operator[] (int i) { return &data[i * m];}
+    const ll* operator[] (int i) const { return &data[i * m]; }
+    Matrix operator*(const Matrix& other) const { 
+        Matrix res(n, other.m);
+        matmul(data, other.data, res.data, n, m, other.m);
+        return res;    
+    }
+};
